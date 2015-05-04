ffbe3a230cf64c6bf990abe3cd2ec9edb5ac5002
Andrea Fiore <andrea.fiore@composable.io>
2015-05-04 16:52:30 +0100

    Allow to specify a mode for the downloaded file

--- src/AnsibleModules/GetUrl.hs
+++ src/AnsibleModules/GetUrl.hs
@@ -19,6 +19,7 @@ data GetUrl = GetUrl
             , url_username   :: Maybe T.Text
             , use_proxy      :: Maybe Bool
             , validate_certs :: Maybe Bool
+            , mode           :: Maybe T.Text
             }
 
 $(A.deriveToJSON encodingOptions ''GetUrl)
@@ -26,24 +27,24 @@ $(A.deriveToJSON encodingOptions ''GetUrl)
 instance ModuleCall GetUrl where
   moduleLabel _ = "get_url"
 
-defaultGetUrl :: URI -> FilePath -> GetUrl
-defaultGetUrl url' dest' = GetUrl
-                         { dest           = dest'
-                         , force          = Just False
-                         , sha256sum      = Nothing
-                         , timeout        = Nothing
-                         , url            = url'
-                         , url_password   = Nothing
-                         , url_username   = Nothing
-                         , use_proxy      = Just True
-                         , validate_certs = Just True
-                         }
+defaultGetUrl :: URI -> FilePath -> Maybe T.Text -> GetUrl
+defaultGetUrl url' dest' m = GetUrl
+                           { dest           = dest'
+                           , force          = Just False
+                           , sha256sum      = Nothing
+                           , timeout        = Nothing
+                           , url            = url'
+                           , url_password   = Nothing
+                           , url_username   = Nothing
+                           , use_proxy      = Just True
+                           , validate_certs = Just True
+                           , mode           = m
+                           }
 
-download :: URI -> FilePath -> CompiledModuleCall
-download url' dest' = compile $ defaultGetUrl url' dest'
+download :: URI -> FilePath -> Maybe T.Text -> CompiledModuleCall
+download url' dest' m = compile $ defaultGetUrl url' dest' m
 
-downloadTask :: URI -> FilePath -> Task
-downloadTask u d =
+downloadTask :: URI -> FilePath -> Maybe T.Text -> Task
+downloadTask u d m =
   task ("downloading " <> T.pack (show u))
-       (download u d)
-
+       (download u d m)
